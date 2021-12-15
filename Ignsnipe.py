4ed1a245eb42149fd160c187dddaffa6432e73de
iTrojang <95707186+iTrojang@users.noreply.github.com>
2021-12-15 18:03:45 +0000

    Update Ignsnipe.py

--- Ignsnipe.py
+++ Ignsnipe.py
@@ -2,15 +2,8 @@ import webbrowser
 from random import choice
 from string import ascii_uppercase
 import requests
-import random
-import string
 import tkinter as tk
 import string
-import schedule
-import selenium
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-import time
 
 
 
@@ -24,8 +17,12 @@ def RandomNameGen(): #pyinstaller.exe --onefile IgnSnipe.py
     try:
         rdat = rs.json()
         print(rdat["name"] + " is Taken")
+        write2 = open("namestaken.txt", "a")
+        write2.write("\n" + rdat["name"] + " is Taken")
     except:
         print(f"{RandomNames} is Not Taken")
+        write =  open("names.txt", "a")
+        write.write(f"\n{RandomNames} is Not Taken")
 
 def CheckAvaiblity():
     try:
@@ -41,7 +38,7 @@ def CheckAvaiblity():
 window = tk.Canvas(height=700, width=700)
 window.pack()
 
-bg = tk.PhotoImage(file="creep.png")
+bg = tk.PhotoImage(file="C:/Users/User/Desktop/app/creep.png")
 
 mylabel = tk.Label(root, image=bg)
 mylabel.place(x=0,y=0, relwidth=1, relheight=1)
@@ -49,7 +46,7 @@ mylabel.place(x=0,y=0, relwidth=1, relheight=1)
 Button1= tk.Button(root,text="Pick Random IGN", padx=10,pady=5,fg="purple",bg="#e71f82", command=RandomNameGen)
 Button1.pack()
 
-Button12= tk.Button(root,text="Check Availability", padx=10,pady=5,fg="purple",bg="#e71f82", command=CheckAvaiblity)
+Button12= tk.Button(root,text="Check Avaiblity", padx=10,pady=5,fg="purple",bg="#e71f82", command=CheckAvaiblity)
 Button12.pack()
 
 
